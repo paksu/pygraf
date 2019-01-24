82c8baf81e72137d1be38ff26237ed8bb14e872e
Simon Fraser <sfraser@mozilla.com>
2019-01-24 10:54:12 +0000

    Fix flake8 for <3.5

--- telegraf/context.py
+++ telegraf/context.py
@@ -11,7 +11,7 @@ from time import time
 def wrapped_coroutine(self, func):
     """Timing wrapper for async functions."""
     @wraps(func)
-    async def wrapped_co(*args, **kwargs):
+    async def wrapped_co(*args, **kwargs):  # noqa E999
         start = time()
         try:
             return await func(*args, **kwargs)
